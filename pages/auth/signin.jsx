ebc5f365dec6885cc6132173c16256566fc69efa
cauanzz <chunfsmw07@gmail.com>
2023-10-09 23:24:08 -0300

    pequenos ajustes no signIn

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -1,62 +1,54 @@
-import { doc, getDoc, setDoc, serverTimestamp } from 'firebase/firestore'
-import { db } from '../../firebase'
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth'
-import { useRouter } from 'next/router'
-import { useRecoilState } from "recoil";
-import { userState } from "../../atom/userAtom";
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { userState } from '../../atom/userAtom';
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
 
 export default function signIn() {
-
-    const routere = useRouter()
-
+  const router = useRouter();
   const [currentUser, setCurrentUser] = useRecoilState(userState);
 
-  if(currentUser) {
-    routere.push('/aluno/dashboard')
-}
-  console.log(currentUser);
-  const auth = getAuth();
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, async (user) => {
       if (user) {
-        const fetchUser = async () => {
-          const docRef = doc(db, "users", auth.currentUser.providerData[0].uid);
-          const docSnap = await getDoc(docRef);
-          if (docSnap.exists()) {
-            setCurrentUser(docSnap.data());
-          }
-        };
-        fetchUser();
+        const docRef = doc(db, 'users', user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setCurrentUser(docSnap.data());
+        }
       }
     });
   }, []);
 
-    const router = useRouter()
-    const handleGoogleSignIn = async () => {
-        try {
-            const auth = getAuth()
-            const provider = new GoogleAuthProvider()
-            await signInWithPopup(auth, provider)
-            const user = auth.currentUser.providerData[0]
-            const docRef = doc(db, "users", user.uid)
-            const docSnap = await getDoc(docRef)
-            if (!docSnap.exists()) {
-                await setDoc(docRef, {
-                    name: user.displayName,
-                    email: user.email,
-                    userImg: user.photoURL,
-                    uid: user.uid,
-                    timestamp: serverTimestamp(),
-                })
-            }
-            router.push('/aluno/dashboard')
-        } catch (error) {
-            console.log(error)
-        }
-    }
+  const handleGoogleSignIn = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+      const user = auth.currentUser.providerData[0];
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
 
-    return (
-        <button onClick={handleGoogleSignIn}>Sign</button>
-    )
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          userImg: user.photoURL,
+          uid: user.uid,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      router.push('/aluno/dashboard');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return <button onClick={handleGoogleSignIn}>Sign</button>;
 }
